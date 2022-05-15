16006d79d70a10d1940cfb1c0459b2f573ebf96e
Julio Muller <juliodolago@gmail.com>
2022-05-14 23:47:05 -0300

    feat: Configure font and base document component

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
