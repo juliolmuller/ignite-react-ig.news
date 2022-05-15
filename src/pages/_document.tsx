6850078c9d83d5456ae288448c7dc3266a56985b
Julio Muller <juliodolago@gmail.com>
2022-05-14 23:56:30 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,7 @@ export default class Document extends NextDocument {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap"
           />
+          <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         </Head>
         <body>
           <Main />
