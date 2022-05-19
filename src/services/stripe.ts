790942bea39592db6eb553f376c1d3c29c951bad
Julio Muller <juliodolago@gmail.com>
2022-05-19 19:37:53 -0300

    fix: change import mode from 'package.json' file

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,10 +1,13 @@
 import Stripe from 'stripe';
 
-import { name, version } from '~/../package.json';
+import projectMeta from '~/../package.json';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2020-08-27',
-  appInfo: { name, version },
+  appInfo: {
+    name: projectMeta.name,
+    version: projectMeta.version,
+  },
 });
 
 export default stripe;
