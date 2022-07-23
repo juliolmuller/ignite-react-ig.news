39f60dc0fd8759964b76d6d391ddfdc0eb617e35
Julio Muller <juliodolago@gmail.com>
2022-07-22 23:55:13 -0300

    test: add configuration to resolve CSS modules

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,12 @@
 import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^.+\\.(gif|ico|jpe?g|mp3|mp4|png|svg|ttf|wav|webm|webp|woff|woff2)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '^.+\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
   setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
