f9f6f722335521c012032f2ad609cf8a43d4305f
Julio Muller <juliodolago@gmail.com>
2022-07-23 01:22:53 -0300

    test: allow parameters to be passed to mocked function/hook

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from '@testing-library/react';
 
+import { useSessionMocked } from '~/tests/mocks/libs';
+
 import Header from '.';
 
 describe('component Header', () => {
   it('renders correctly', () => {
+    useSessionMocked(false);
     render(<Header />);
 
     expect(screen.getByText('Home')).toBeInTheDocument();
--- src/tests/mocks/libs.ts
+++ src/tests/mocks/libs.ts
@@ -1,11 +1,22 @@
+import { mocked } from 'jest-mock';
+import { useSession } from 'next-auth/react';
+
 jest.mock('next/router', () => ({
   useRouter: () => ({
     asPath: '/',
   }),
 }));
 
-jest.mock('next-auth/react', () => ({
-  useSession: () => ({}),
-}));
+jest.mock('next-auth/react');
 
-export {}; // only to pass through TS compiler "isolateModule" rule
+export function useSessionMocked(authenticated: boolean) {
+  return authenticated
+    ? mocked(useSession).mockReturnValueOnce({
+        status: 'authenticated',
+        data: { user: { name: 'John Doe' }, expires: '' },
+      })
+    : mocked(useSession).mockReturnValueOnce({
+        status: 'unauthenticated',
+        data: null,
+      });
+}
