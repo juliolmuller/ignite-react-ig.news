9f783556be210b019ed3c3c0a9c31ea43c95a255
Julio Muller <juliodolago@gmail.com>
2022-07-24 01:06:16 -0300

    test: add tests for "SubscribeButton" component

---
+++ src/components/SubscribeButton/SubscribeButton.test.tsx
@@ -0,0 +1,71 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { mocked } from 'jest-mock';
+import { signIn, useSession } from 'next-auth/react';
+
+import { getStripe } from '~/services/client/stripe';
+
+import SubscribeButton from '.';
+
+jest.mock('next-auth/react');
+jest.mock('~/services/client/stripe');
+
+describe('component SubscribeButton', () => {
+  it('renders and work correctly when user is not authenticated', () => {
+    mocked(useSession).mockReturnValueOnce({
+      status: 'unauthenticated',
+      data: null,
+    });
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Subscribe now');
+    const signInMocked = mocked(signIn);
+    fireEvent.click(subscribeButton);
+
+    expect(subscribeButton).toBeInTheDocument();
+    expect(signInMocked).toHaveBeenCalled();
+  });
+
+  it('renders and work correctly when user is authenticated but not subscribe', async () => {
+    mocked(useSession).mockReturnValueOnce({
+      status: 'authenticated',
+      data: { user: { name: 'John Doe' }, expires: '' },
+    });
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Subscribe now');
+    const subscriptionCall = jest.fn(() => Promise.resolve({}));
+    const checkoutCall = jest.fn();
+    Object.defineProperty(window, 'fetch', {
+      value: () => Promise.resolve({ json: subscriptionCall }),
+    });
+    mocked(getStripe).mockResolvedValueOnce({
+      redirectToCheckout: checkoutCall,
+    } as any);
+    fireEvent.click(subscribeButton);
+
+    /* eslint-disable testing-library/no-wait-for-multiple-assertions */
+    await waitFor(() => {
+      expect(subscribeButton).toBeInTheDocument();
+      expect(subscriptionCall).toHaveBeenCalled();
+      expect(checkoutCall).toHaveBeenCalled();
+    });
+    /* eslint-enable */
+  });
+
+  it('renders and work correctly when user is authenticated and subscribe', () => {
+    mocked(useSession).mockReturnValueOnce({
+      status: 'authenticated',
+      data: {
+        user: { name: 'John Doe' },
+        activeSubscription: 'fake-subscription-id',
+        expires: '',
+      },
+    });
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Read latest posts');
+
+    expect(subscribeButton).toBeInTheDocument();
+    expect(subscribeButton).toHaveAttribute('href', '/posts');
+  });
+});
