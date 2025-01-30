c4e55a981a59d89812a9a380afefe7adbd97bdc9
juliolmuller <juliolmuller@outlook.com>
2025-01-30 01:10:38 -0300

    fix: fix broken types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
 import '~/styles.scss';
 import Header from '~/components/Header';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export type MyApProps = AppProps<{
+  session: Session;
+}>;
+
+export default function MyApp({ Component, pageProps }: MyApProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Header />
