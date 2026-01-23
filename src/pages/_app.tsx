21de99ce10c9eb59deb7a1b58cd85795f2fd954c
juliolmuller <juliolmuller@outlook.com>
2026-01-23 17:41:46 -0300

    style: fix imports order
    Fix as per CodeRabbit AI review comment at https://github.com/juliolmuller/ignite-react-ig.news/pull/2#discussion_r2722327997.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { type AppProps } from 'next/app';
+import { type ReactNode } from 'react';
 
 import '~/styles.scss';
-import { type ReactNode } from 'react';
 
 import Header from '~/components/Header';
 
